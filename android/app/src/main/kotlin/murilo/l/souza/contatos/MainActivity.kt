594f0d20fb80b182662b735278b8bd9307003e6b
murilo-souza <murilo.l.souza232@gmail.com>
2021-06-04 15:51:38 -0300

    Final

---
+++ android/app/src/main/kotlin/murilo/l/souza/contatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package murilo.l.souza.contatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:contatos/ui/contact_page.dart';
+import 'package:flutter/material.dart';
+import 'package:contatos/ui/Home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
